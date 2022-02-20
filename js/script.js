963c2a1d6fb028b33ec4387d767c829fc947977f
Gias uddin vuiya <giasuddin6900@gmail.com>
2022-02-20 15:56:41 +0600

    my final github project virsion - 0.1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let numberOne = 40;
+let numberTwo = 400;
+let numberThree = 4000;
+
+if(numberOne !== numberOne || numberThree >= numberTwo)
+{
+	console.log('I will be successful person in the world do you believe it??? ');
+}
+	else 
+{
+	console.log('I will be failer person.');
+}
+
+
